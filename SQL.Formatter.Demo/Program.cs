9ee5132a72d2341470a8e20dacc84e7b087ad353
hogimn <hogimn@gmail.com>
2023-09-24 10:44:46 +0900

    Update ASP.NET Core Logging to Serilog

--- SQL.Formatter.Demo/Program.cs
+++ SQL.Formatter.Demo/Program.cs
@@ -1,8 +1,14 @@
 using Microsoft.AspNetCore.HttpLogging;
 using Microsoft.Extensions.Options;
 using System.Net;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 builder.WebHost.ConfigureKestrel(options =>
 {
@@ -23,6 +29,8 @@ builder.Services.AddHttpLogging(logging =>
 {
     logging.LoggingFields = HttpLoggingFields.All;
 });
+
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 

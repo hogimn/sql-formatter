1b147ec15a486f360105bc256ee328392e061eb6
hogimn <hogimn@gmail.com>
2024-09-19 11:13:58 +0900

    Fix: Change public to PUBLIC in TSqlFormatter keywords

--- SQL.Formatter/Language/TSqlFormatter.cs
+++ SQL.Formatter/Language/TSqlFormatter.cs
@@ -11,7 +11,7 @@ namespace SQL.Formatter.Language
         "PROCEDURE",
         "ALL",
         "FETCH",
-        "public",
+        "PUBLIC",
         "ALTER",
         "FILE",
         "RAISERROR",
